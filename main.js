0f24db286f1a2f06f1defc41b0246ff886f113a6
Laiba <96579061+CoderKid01@users.noreply.github.com>
2022-10-29 13:13:51 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function modelReady(){  classifier.classify( gotResults);}
+
+function startClassification(){navigator.mediaDevices.getUserMedia({audio:true});classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/kA4F-80yW/model.json', modelReady);}
